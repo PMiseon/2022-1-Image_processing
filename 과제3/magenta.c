784bbae73f4f7144c4bc92892f8a4b6835c6deb6
Miseon Park <106222104+PMiseon@users.noreply.github.com>
2023-07-02 00:39:38 +0900

    과제3 magenta.c

---
+++ 과제3/magenta.c
@@ -0,0 +1,15 @@
+//color2gray.c 파일을 이용하였습니다.
+//for(i=0;i<(number_of_pixels);i++) 안의 내용을 수정하였으며, 수정된 부분만 첨부하였습니다.
+//첨부된 내용에는 없지만 red, green, blue, c, m, y 를 float 형태로 선언하여 사용했습니다.
+for (i = 0; i < (number_of_pixels); i++) {
+red = (float)buffer[3 * i] / 255;
+green = (float)buffer[3 * i + 1] / 255;
+blue = (float)buffer[3 * i + 2] / 255;
+//RGB’ = RGB 0<=RGB<=1 값으로 만들기
+c = 1 - red; //cyan
+m = 1 - green; //magenta
+y = 1 - blue; //yellow
+temp = 255 * m; //(256 * 3 * m)/ 3 
+CLIP(temp, 0, 255);
+obuffer[i] = temp; // Generates output buffer for Brightness component
+}
